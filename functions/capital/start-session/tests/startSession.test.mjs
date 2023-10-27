2eebc4e7edb0f12e7897f4d11794b14eeb3f2e05
George Kpai <koatekpai.dev@gmail.com>
2023-10-27 13:05:09 +0100

    add more tests

--- functions/capital/start-session/tests/startSession.test.mjs
+++ functions/capital/start-session/tests/startSession.test.mjs
@@ -25,4 +25,31 @@ describe('doStartSession unit tests', () => {
         expect(result.session.CST).to.equal('1234567890');
         expect(result.session.TOKEN).to.equal('qwertyuiopasdfghjkl');
       });
+
+      it('should return an error response with the Capital.com error code when the start session request fails', async () => {
+        // -> GIVEN
+        const errMsg = { "errorCode": "error.invalid.details" };
+        const blob = new Blob([JSON.stringify(errMsg, null, 2)], { type: "application/json" });
+        sinon.stub(global, 'fetch').callsFake(() => new Response(blob, {
+          status: 401,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }));
+        // -> WHEN
+        const result = await doStartSession(event, 'http://endpoint.com', 'DEMO-KEY', 'name@email.com', 'abc-123-!');
+        // -> THEN
+        expect(result.message).to.include(`error.invalid.details`);
+      });
+    
+      it('should return the error object when the start session request throws an error', async () => {
+        // -> GIVEN
+        sinon.stub(global, 'fetch').throws(new Error('Network error'));
+        // -> WHEN
+        const result = await doStartSession(event, 'http://endpoint', 'DEMO-KEY', 'name@email.com', 'abc-123-!');
+        // -> THEN
+        expect(result.message).to.include('Could not fetch with the following parameters:');
+        expect(result.message).to.include('DEMO-KEY');
+        expect(result.message).to.include('name@email.com');
+      });
 })
