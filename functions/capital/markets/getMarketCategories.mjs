41af675b53712044b76c5600a60bd63056ef694f
George Kpai <koatekpai.dev@gmail.com>
2023-10-30 15:59:32 +0000

    create getMarketCategories feature

---
+++ functions/capital/markets/getMarketCategories.mjs
@@ -0,0 +1,33 @@
+/*global fetch*/
+import { getParameter } from '../../opt/parameterStore.mjs';
+
+const CT_DEMO_URL = process.env.CT_DEMO_URL;
+const ENDPOINT = process.env.MARKET_CATEGORIES_ENDPOINT
+
+export const handler = async (event) => {
+  const BASE_URL = await getParameter(CT_DEMO_URL, false);
+  const url = BASE_URL + ENDPOINT;
+  const session = event.session;
+  const headers = {
+    CST: session.CST,                       //-> CST is retrieved from the return object of GetSession state machine
+    'X-SECURITY-TOKEN': session.TOKEN       //-> TOKEN is retrieved from the return object of GetSession state machine
+  }
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: headers
+  })
+
+  if (response.status == 200) {
+    return await response.json(); 
+    //-> Format of this json: https://open-api.capital.com/#tag/Markets-Info-greater-Markets/paths/~1api~1v1~1marketnavigation/get
+  }
+
+  const errCode = await response.json();
+  return {
+    message: `
+          Error getting top market categories from capital.com
+          Capital.com Error Code: ${errCode?.errorCode}
+        `
+  }
+}
